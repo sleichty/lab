ab5b8468086565accfe3b37ac24c376405345a46
sleichty <sleichty@iastate.edu>
2016-10-19 10:15:10 -0500

    script changes

--- week_08/data_cleaning/script.R
+++ week_08/data_cleaning/script.R
@@ -2,19 +2,22 @@
 
 #First, read it in to make it a dataframe with one header
 #Select the line with the most useful information 
-header_weather<-scan("weather.txt")
+header_weather<-scan("weather.txt", skip = 1, nlines = 1, what = character(), sep = "\t")
 #Select all the data
-weather<-read.table("weather.txt")
+weather<-read.table("weather.txt", skip = 4, na.strings = "?", sep = "\t", stringsAsFactors = FALSE)
 #Put the header and the data together
 names(weather)<-header_weather
 
 #Lets just work with time, total rainfall, total solar radiation, max air temp, and min air temp -- select these columns
 
+weather[,c("TIMESTAMP", "Rain_in_Tot", "SlrMJ_Tot", "AirTF_Max", "AirTF_Min")]
+cleandata<-weather
 #Give the column new names -- "date", "total_rain_in", "total_rad_MJ", "max_temp_F", "min_temp_F"
-
+colnames(cleandata)<-c("date", "total_rain_in", "total_rad_MJ", "max_temp_F", "min_temp_F")
+head(cleandata)
 #Filter the data to make sure you have reasonable values
   #Between 0 and 6 for total rain
-  #Between 60 and 100 for total radiation
+  #Between 30 and 70 for total radiation
   #Between -50 and -10 for both temps
   #max temp cannot be lower than min temp
 
